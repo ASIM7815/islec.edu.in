6aa4aeed30aad1b4baf07ea49d21cb80f09010a6
asimsaad <mohammadasimsaad@gmail.com>
2026-01-27 11:48:53 +0530

    ....

--- new.js
+++
@@ -1,6 +0,0 @@
-(function () {
-    var script = document.createElement("script");
-    script.src = "asset/static/js/main.64d07445.js";
-    script.defer = true;
-    document.head.appendChild(script);
-})();
